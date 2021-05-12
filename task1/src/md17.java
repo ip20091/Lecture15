5b2c3b5ec82a810d0b0218356de9cb6fbc52bdf4
ip20091 <Zoom.test.12>
2021-05-12 18:38:15 +0300

    Forth commit

---
+++ task1/src/md17.java
@@ -0,0 +1,12 @@
+public class md17 {
+
+    @data
+    public class BookLombok {
+
+        private String author;
+        private String bookName;
+        private int year;
+        private String genre;
+        private String isbn;
+    }
+}
